20be62b7d2bc946281cd27bb372c3905d31ed067
TPWigl <157946840+TPWigl@users.noreply.github.com>
2024-01-28 01:09:10 -0800

    Update app(api).js

--- scripts/app(api).js
+++ scripts/app(api).js
@@ -2,7 +2,7 @@ class Forcast{
     constructor(){
         this.apiKey='pAdHBYCVwCmsjvEr63oUG9XRGLVLjpJO'
         this.weatherURI='https://dataservice.accuweather.com/currentconditions/v1/'
-        this.cityURI='http://dataservice.accuweather.com/locations/v1/cities/search'
+        this.cityURI='https://dataservice.accuweather.com/locations/v1/cities/search'
     }
     async updateCity(city){
         const cityData = await this.getCity(city)
